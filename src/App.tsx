ed566b1ef3f6f47a738a62a0484574e376a42fa6
Mokiwara-D <samhmcq@gmail.com>
2025-07-04 12:30:21 +0100

    Refactor font management and CSS variables: move GothamSSm font definitions to a new fonts.css file, update index.css to use CSS variables for colors, and import new styles in App.tsx.

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,6 @@
 import './App.css'
+import './styles/fonts.css'
+import './styles/variables.css'
 import Header from './components/Header/Header'
 import SearchBar from './components/SearchBar/SearchBar'
 import Footer from './components/Footer/Footer'
